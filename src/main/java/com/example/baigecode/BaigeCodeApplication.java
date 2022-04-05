e8f2057f104f633dfa42eef97eb586a7eaed2e41
Akzhol Issaliyev <akzholbusiness@gmail.com>
2022-04-05 15:23:27 +0600

    spring skeleton

---
+++ src/main/java/com/example/baigecode/BaigeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.baigecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaigeCodeApplication.class, args);
+    }
+
+}
